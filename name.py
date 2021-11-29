eff8e8d0b9eacfb36de59480501f3b17b4400482
Paulo Braga <94999843+PaloBraga@users.noreply.github.com>
2021-11-29 12:59:26 -0300

    Add files via upload
    atualização

--- name.py
+++ name.py
@@ -9,3 +9,37 @@ print(name.upper())
 name="ada lovElace"
 
 print(name.lower())
+
+first_name="paulo"
+last_name="braga"
+full_name= first_name +" marcelo da silva "+ last_name
+
+print(full_name)
+
+mensagem="Hello, "+ full_name.title() + "!"
+
+print(mensagem)
+
+print("\tPython")
+
+print("Languages:\nPython\nC\nJavaScript")
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+favorite_languege='python '
+
+favorite_languege=favorite_languege.rstrip() 
+
+print(favorite_languege)
+
+favorite_languege=' python'
+
+favorite_languege=favorite_languege.lstrip() 
+
+print(favorite_languege)
+
+favorite_languege=' python '
+
+favorite_languege=favorite_languege.strip() 
+
+print(favorite_languege)
