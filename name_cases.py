68ff5e184bcceeb0452376c5104792725bc420c4
Paulo Braga <94999843+PaloBraga@users.noreply.github.com>
2021-12-02 16:24:03 -0300

    Add files via upload
    exercício da pág.: 61.

---
+++ name_cases.py
@@ -0,0 +1,22 @@
+pessoa="Paulo, "
+
+print("Alô "+ pessoa + "você gostaria de aprender um pouco de Python hoje?")
+
+print(pessoa.lower())
+
+print(pessoa.upper())
+
+print(pessoa.title())
+
+print('Rocky Balboa, certa vez disse: “O mundo não é um grande arco-íris, é um lugar sujo e cruel que nao quer saber o quanto você é durão, vai colocar você de joelhos e você ficará de joelhos para sempre se você deixar. Você, eu, ninguem vai bater tão duro quanto a vida, mas nao se trata de bater duro, se trata de quanto você aguenta apanhar e seguir em frente, o quanto você é capaz de tentar e continuar tentando! É assim que se consegue vencer!”')
+
+famous_person="\n\tRocky Balboa, "
+
+mensagem="certa vez disse: “O mundo não é um grande arco-íris, é um lugar sujo e cruel que nao quer saber o quanto você é durão, vai colocar você de joelhos e você ficará de joelhos para sempre se você deixar. Você, eu, ninguem vai bater tão duro quanto a vida, mas nao se trata de bater duro, se trata de quanto você aguenta apanhar e seguir em frente, o quanto você é capaz de tentar e continuar tentando! É assim que se consegue vencer!”"
+print(famous_person + mensagem)
+
+print(pessoa.lstrip())
+
+print(pessoa.rstrip())
+
+print(pessoa.strip())
