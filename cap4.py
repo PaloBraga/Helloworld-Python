b495fecf035d4ce053cc957db8b273f546484e33
Paulo Braga <94999843+PaloBraga@users.noreply.github.com>
2021-12-21 13:58:51 -0300

    Add files via upload

--- cap4.py
+++ cap4.py
@@ -2,7 +2,7 @@ magicos=["Paulo", "Nemezio", "Felix"]
 
 for magico in magicos:
 
-print(magico)
+    print(magico)
 
+# ESSE LIVRO QUE ME ENDOIDA, DEU ERRO, ERRO, AÍ COM UM "TAB" ANTES NO PRINT, TUDO FOI RESOLVIDO(ISSO O LIVRO NÃO DIZ).
 
-# ESSE LIVRO QUE ME ENDOIDA, PAG 87, 88 NÃO ENTENDO...
