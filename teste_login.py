d40155bff6f17dfd7bd64e7c577ddf1500497490
Jorbson Luiz <95379637+Jorbsono@users.noreply.github.com>
2021-12-21 15:53:42 -0300

    teste_login

--- teste_login.py
+++ teste_login.py
@@ -1,15 +1,25 @@
+#DICIONARIO DE USUARIOS.
 logins = {
-"jorbson": { "nome" : "jorbson" , "senha" : "1234" },
-"neto"   : { "nome" :  "neto"   , "senha" : "2345" },
-"paulo"  : { "nome" :  "paulo"  , "senha" : "3456" },
+"jorbson"   : { "nome" :  "jorbson"   , "senha" : "#print" },
+"neto"      : { "nome" :  "neto"      , "senha" : "nice"   },
+"paulo"     : { "nome" :  "paulo"     , "senha" : "tobebo" },
+"guilherme" : { "nome" :  "guilherme" , "senha" : "tampico"},
+"eduardo"   : { "nome" :  "eduardo"   , "senha" : "drogas" },
+"ricardo"   : { "nome" :  "ricardo"   , "senha" : "51pura" },
+"adelson"   : { "nome" :  "adelson"   , "senha" : "0000"   },
+"gabriel"   : { "nome" :  "gabriel"   , "senha" : "meve5"  },
 }
 
+#LOGIN VAI RECEBER O QUE FOR DIGITADO.
 login = input('digite o login:')
 
+#SENHA VAI RECEBER O QUE FOR DIGITADO.
 senha = input('digite a senha:')
 
+#VARIAVEL DEFINIDA COMO FALSA.
 usuario_existe = False
 
+#VERIFICANDO SE O LIGIN QUE FOI DIGITADO EXISTE NO DICIONARIO COMO USUARIO.
 for usuario in logins:
     if login == usuario:
         usuario_existe = True
@@ -19,13 +29,13 @@ if usuario_existe:
     usuario = logins[login]
 
 
-    #if senha == logins[login]["senha"]:
+    #VERIFICANDO SE A SENHA DO USUARIO ESTÁ CORRETA.
     if senha == usuario["senha"]:
         print ("bem vindo " + usuario["nome"])
 
     else:
         print("Senha invalida")
  
+#USUARIO NÃO FOI ENCONTRADO NO DICIONARIO DE USUARIOS.
 else:
-    print("usuario não existe " + login)
-    
+    print("usuario" " " + login + " " "não está cadastrado")
