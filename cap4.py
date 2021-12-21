75e31a35b253426ac72d908ac2a2c609af3f51b1
Paulo Braga <94999843+PaloBraga@users.noreply.github.com>
2021-12-21 16:22:51 -0300

    Add files via upload

--- cap4.py
+++ cap4.py
@@ -3,6 +3,27 @@ magicos=["Paulo", "Nemezio", "Felix"]
 for magico in magicos:
 
     print(magico)
+print("")
 
 # ESSE LIVRO QUE ME ENDOIDA, DEU ERRO, ERRO, AÍ COM UM "TAB" ANTES NO PRINT, TUDO FOI RESOLVIDO(ISSO O LIVRO NÃO DIZ).
+
+gatos=["pantera", "Lok", "feifei"]
+
+for gato in gatos:
+    print(gato)
+print("")
+
+cachorros=["thor", "teo", "biscoito"]
+
+for cachorro in cachorros:
+    print(cachorro)
+print("")
+
+ferramentas=["alicate", "chave filips", "multimetro"]
+
+for ferramenta in ferramentas:
+    print(ferramenta.title())
+print("")
+
+#PAG 89
 
