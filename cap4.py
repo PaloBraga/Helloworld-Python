5997b673d9c148a78b16b8e0313df45bd94b0fb6
Paulo Braga <94999843+PaloBraga@users.noreply.github.com>
2021-12-27 15:23:12 -0300

    Add files via upload

--- cap4.py
+++ cap4.py
@@ -2,7 +2,8 @@ magicos=["Paulo", "Nemezio", "Felix"]
 
 for magico in magicos:
 
-    print(magico)
+
+    print(magico.title()+", isso é um otimo truque!")
 print("")
 
 # ESSE LIVRO QUE ME ENDOIDA, DEU ERRO, ERRO, AÍ COM UM "TAB" ANTES NO PRINT, TUDO FOI RESOLVIDO(ISSO O LIVRO NÃO DIZ).
@@ -10,13 +11,13 @@ print("")
 gatos=["pantera", "Lok", "feifei"]
 
 for gato in gatos:
-    print(gato)
+    print(gato.title()+", você só dorme?")
 print("")
 
 cachorros=["thor", "teo", "biscoito"]
 
 for cachorro in cachorros:
-    print(cachorro)
+    print(cachorro.title()+ ", hoje é dia de lata!")    
 print("")
 
 ferramentas=["alicate", "chave filips", "multimetro"]
@@ -26,4 +27,19 @@ for ferramenta in ferramentas:
 print("")
 
 #PAG 89
+
+magicos=["Paulo", "Nemezio", "Felix"]
+
+for magico in magicos:
+
+
+    print("Estamos ansioso para o proximo truque, "+ magico.title()+"!\n")
+    print(magico.title()+", isso é um otimo truque!\n")
+
+#PAG 90
+for magico in magicos:
+    print(magico.title()+ ", foi um ótimo truque! \n")
+    print("Mal posso esperar para ver seu próximo truque, "+ magico.title()+ "!\n")
+
+#PAG 91
 
