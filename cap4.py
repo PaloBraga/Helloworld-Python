9ddec8895e02b274bf436b194b053cbb5449a3fc
Paulo Braga <94999843+PaloBraga@users.noreply.github.com>
2022-01-08 10:37:34 -0300

    Add files via upload
    atualização

--- cap4.py
+++ cap4.py
@@ -41,5 +41,33 @@ for magico in magicos:
     print(magico.title()+ ", foi um ótimo truque! \n")
     print("Mal posso esperar para ver seu próximo truque, "+ magico.title()+ "!\n")
 
-#PAG 93
+#PAG 94 - EXERCICIO
+
+pizzas=["calabresa", "frango", "carne de sol"]
+
+for pizza in pizzas:
+    print(pizza)
+    print("")
+
+for adoro in pizzas:
+    print("Eu adoro pizza de " +adoro.title())
+    print("")
+
+animais=["cão", "gato", "rato"]
+
+for animal in animais:
+    print("Um "+ animal +", é um otimo animal!")
+
+#PAG 95
+
+for valor in range(1,5):
+    print(valor)
+print("")
+#ESTE COMANDO IMPRIME OS NÚMEROS DO 1 AO 4, CASO QUEIRA IMPRIMIR OS 5 OU MAIS, COLOCAR SEMPRE UM NÚMERO A MAIS.
+for valor in range(1,6):
+    print(valor)
+print("")
+#AGORA APÓS ADICIONAR UM NÚMERO A MAIS ELE IMPRIMI DO 1 AO 5.
+
+#PAG 96
 
