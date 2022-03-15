3c20eb4ca61d02431781d8813eeb57de9a8a77c6
Paulo Braga <94999843+PaloBraga@users.noreply.github.com>
2022-03-15 09:36:07 -0300

    Add files via upload

--- Cap5.py
+++ Cap5.py
@@ -1,2 +1,9 @@
 #CAP 5 PAGINA 110
 
+carros=["audi", "bmw", "Subaru", "toyota"]
+for carro in carros:
+    if carro == "bmw":
+        print(carro.upper()) else:
+        print(carro.title())
+
+#ESSE LIVRO VAI ME EM DOIDA, PORQUE ALGUNS EXEMPLOS NÃO FUNCIONAM.
